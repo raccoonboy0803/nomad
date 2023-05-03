417833274486e2ae3ba09c846331dd3b07b7d7f5
raccoonboy <raccoonboy0803@gmail.com>
2023-05-04 00:10:42 +0900

    #5 CRYPTO TRACKER - 5.8 Nested Routes part Two

---
+++ react-masterclass/src/Router.tsx
@@ -0,0 +1,20 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Chart from './routes/Chart';
+import Coin from './routes/Coin';
+import Coins from './routes/Coins';
+import Price from './routes/Price';
+
+const Router = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Coins />} />
+        <Route path="/:coinId/*" element={<Coin />}>
+          {/* <Route path="price" element={<Price />} />
+          <Route path="chart" element={<Chart />} /> */}
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+};
+export default Router;
--- react-masterclass/src/index.tsx
+++ react-masterclass/src/index.tsx
@@ -2,13 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { ThemeProvider } from 'styled-components';
 import App from './App';
-import { darkTheme, lightTheme } from './theme';
+import { theme } from './theme';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <ThemeProvider theme={lightTheme}>
+  <ThemeProvider theme={theme}>
     <App />
   </ThemeProvider>
 );
